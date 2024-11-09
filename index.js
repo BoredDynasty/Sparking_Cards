2dadf91647bea765ac7c0ec1728914db8213a9d2
Dynasty <dynastysuperform@gmail.com>
2024-11-08 21:43:38 -0700

    index.js Fixes

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var body = document.body;
 
+// Defining Variables
+var linkBG = "#1446ff";
+var linkBGHover = "#5c7af1";
+var backgroundColor = "#111216";
+var textColor = "white";
+var borderColor = "#2c2e34";
+
 // Headers
 var welcomeHeader = document.getElementById("cards_welcome");
 var welcomeP = document.getElementById("cards_p");
@@ -14,11 +21,13 @@ var githubLink = document.getElementById("github_link");
 githubLink.textContent = "Source Code";
 githubLink.classList.add("link_style_1");
 githubLink.href = "https://github.com/BoredDynasty/Sparking_Cards";
+githubLink.style.backgroundColor = linkBG;
 
 // Images
 var cardsImgHead = document.getElementById("cards_img_head");
-cardsImgHead.src = "https://github.com/BoredDynasty/Sparking_Cards/blob/main/Files/Images/Sparking%20Cards%20Transparent%20$20BG.png?raw=true";
+cardsImgHead.src = "https://github.com/BoredDynasty/Sparking_Cards/blob/web/Files/Images/SparkingCardsTransparerentBG.png?raw=true";
 cardsImgHead.classList.add("img_style_1");
 cardsImgHead.style.position = "absolute";
 cardsImgHead.style.top = "0";
 cardsImgHead.style.left = "0";
+cardsImgHead.style.backgroundColor = "transparent";
